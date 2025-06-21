714a4bca288feaa9f0ab4843881b4679c975bdd2
Michael <theretroanimatedyt@gmail.com>
2025-06-21 17:47:54 -0400

    Updates

--- server/index.js
+++ server/index.js
@@ -157,6 +157,90 @@ const route0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProper
   default: root,
   links
 }, Symbol.toStringTag, { value: "Module" }));
+const logoDark = "/LunaScouter/assets/logo-dark-pX2395Y0.svg";
+const logoLight = "/LunaScouter/assets/logo-light-CVbx2LBR.svg";
+function Welcome() {
+  return /* @__PURE__ */ jsx("main", {
+    className: "flex items-center justify-center pt-16 pb-4",
+    children: /* @__PURE__ */ jsxs("div", {
+      className: "flex-1 flex flex-col items-center gap-16 min-h-0",
+      children: [/* @__PURE__ */ jsx("header", {
+        className: "flex flex-col items-center gap-9",
+        children: /* @__PURE__ */ jsxs("div", {
+          className: "w-[500px] max-w-[100vw] p-4",
+          children: [/* @__PURE__ */ jsx("img", {
+            src: logoLight,
+            alt: "React Router",
+            className: "block w-full dark:hidden"
+          }), /* @__PURE__ */ jsx("img", {
+            src: logoDark,
+            alt: "React Router",
+            className: "hidden w-full dark:block"
+          })]
+        })
+      }), /* @__PURE__ */ jsx("div", {
+        className: "max-w-[300px] w-full space-y-6 px-4",
+        children: /* @__PURE__ */ jsxs("nav", {
+          className: "rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4",
+          children: [/* @__PURE__ */ jsx("p", {
+            className: "leading-6 text-gray-700 dark:text-gray-200 text-center",
+            children: "What's next?"
+          }), /* @__PURE__ */ jsx("ul", {
+            children: resources.map(({
+              href,
+              text,
+              icon
+            }) => /* @__PURE__ */ jsx("li", {
+              children: /* @__PURE__ */ jsxs("a", {
+                className: "group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500",
+                href,
+                target: "_blank",
+                rel: "noreferrer",
+                children: [icon, text]
+              })
+            }, href))
+          })]
+        })
+      })]
+    })
+  });
+}
+const resources = [{
+  href: "https://reactrouter.com/docs",
+  text: "React Router Docs",
+  icon: /* @__PURE__ */ jsx("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "24",
+    height: "20",
+    viewBox: "0 0 20 20",
+    fill: "none",
+    className: "stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300",
+    children: /* @__PURE__ */ jsx("path", {
+      d: "M9.99981 10.0751V9.99992M17.4688 17.4688C15.889 19.0485 11.2645 16.9853 7.13958 12.8604C3.01467 8.73546 0.951405 4.11091 2.53116 2.53116C4.11091 0.951405 8.73546 3.01467 12.8604 7.13958C16.9853 11.2645 19.0485 15.889 17.4688 17.4688ZM2.53132 17.4688C0.951566 15.8891 3.01483 11.2645 7.13974 7.13963C11.2647 3.01471 15.8892 0.951453 17.469 2.53121C19.0487 4.11096 16.9854 8.73551 12.8605 12.8604C8.73562 16.9853 4.11107 19.0486 2.53132 17.4688Z",
+      strokeWidth: "1.5",
+      strokeLinecap: "round"
+    })
+  })
+}, {
+  href: "https://rmx.as/discord",
+  text: "Join Discord",
+  icon: /* @__PURE__ */ jsx("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "24",
+    height: "20",
+    viewBox: "0 0 24 20",
+    fill: "none",
+    className: "stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300",
+    children: /* @__PURE__ */ jsx("path", {
+      d: "M15.0686 1.25995L14.5477 1.17423L14.2913 1.63578C14.1754 1.84439 14.0545 2.08275 13.9422 2.31963C12.6461 2.16488 11.3406 2.16505 10.0445 2.32014C9.92822 2.08178 9.80478 1.84975 9.67412 1.62413L9.41449 1.17584L8.90333 1.25995C7.33547 1.51794 5.80717 1.99419 4.37748 2.66939L4.19 2.75793L4.07461 2.93019C1.23864 7.16437 0.46302 11.3053 0.838165 15.3924L0.868838 15.7266L1.13844 15.9264C2.81818 17.1714 4.68053 18.1233 6.68582 18.719L7.18892 18.8684L7.50166 18.4469C7.96179 17.8268 8.36504 17.1824 8.709 16.4944L8.71099 16.4904C10.8645 17.0471 13.128 17.0485 15.2821 16.4947C15.6261 17.1826 16.0293 17.8269 16.4892 18.4469L16.805 18.8725L17.3116 18.717C19.3056 18.105 21.1876 17.1751 22.8559 15.9238L23.1224 15.724L23.1528 15.3923C23.5873 10.6524 22.3579 6.53306 19.8947 2.90714L19.7759 2.73227L19.5833 2.64518C18.1437 1.99439 16.6386 1.51826 15.0686 1.25995ZM16.6074 10.7755L16.6074 10.7756C16.5934 11.6409 16.0212 12.1444 15.4783 12.1444C14.9297 12.1444 14.3493 11.6173 14.3493 10.7877C14.3493 9.94885 14.9378 9.41192 15.4783 9.41192C16.0471 9.41192 16.6209 9.93851 16.6074 10.7755ZM8.49373 12.1444C7.94513 12.1444 7.36471 11.6173 7.36471 10.7877C7.36471 9.94885 7.95323 9.41192 8.49373 9.41192C9.06038 9.41192 9.63892 9.93712 9.6417 10.7815C9.62517 11.6239 9.05462 12.1444 8.49373 12.1444Z",
+      strokeWidth: "1.5"
+    })
+  })
+}];
+const route1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  Welcome
+}, Symbol.toStringTag, { value: "Module" }));
 const lunatecsImg = "/LunaScouter/assets/lunatecs-DYXrR15G.jpg";
 const githubLogo = "/LunaScouter/assets/Github-Dr8lduzb.png";
 const initialCounters = {
@@ -1162,11 +1246,11 @@ const scouting = UNSAFE_withComponentProps(function Scouting() {
     }), renderTabs(), tab === "scouting" && renderScoutingTab(), tab === "configuration" && renderConfigurationTab(), tab === "data" && renderDataTab()]
   });
 });
-const route1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const route2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   default: scouting
 }, Symbol.toStringTag, { value: "Module" }));
-const serverManifest = { "entry": { "module": "/LunaScouter/assets/entry.client-GYH6YEgi.js", "imports": ["/LunaScouter/assets/chunk-NL6KNZEE-b53XU3gX.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": true, "module": "/LunaScouter/assets/root-DHuZBVG3.js", "imports": ["/LunaScouter/assets/chunk-NL6KNZEE-b53XU3gX.js"], "css": ["/LunaScouter/assets/root-De0Gz5AH.css"], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/scouting": { "id": "routes/scouting", "parentId": "root", "path": void 0, "index": true, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/LunaScouter/assets/scouting-s9gC2W9a.js", "imports": ["/LunaScouter/assets/chunk-NL6KNZEE-b53XU3gX.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 } }, "url": "/LunaScouter/assets/manifest-267c8753.js", "version": "267c8753", "sri": void 0 };
+const serverManifest = { "entry": { "module": "/LunaScouterassets/entry.client-DACUokgY.js", "imports": ["/LunaScouterassets/welcome-D_zvdyIk.js", "/LunaScouterassets/chunk-NL6KNZEE-BxV1z1Wy.js"], "css": [] }, "routes": { "root": { "id": "root", "parentId": void 0, "path": "", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": true, "module": "/LunaScouterassets/root-CskSl29F.js", "imports": ["/LunaScouterassets/welcome-D_zvdyIk.js", "/LunaScouterassets/chunk-NL6KNZEE-BxV1z1Wy.js"], "css": ["/LunaScouterassets/root-DH8-lYN-.css"], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "welcome/welcome": { "id": "welcome/welcome", "parentId": "root", "path": "/", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/LunaScouterassets/welcome-D_zvdyIk.js", "imports": [], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 }, "routes/scouting": { "id": "routes/scouting", "parentId": "root", "path": "/scouting", "index": void 0, "caseSensitive": void 0, "hasAction": false, "hasLoader": false, "hasClientAction": false, "hasClientLoader": false, "hasClientMiddleware": false, "hasErrorBoundary": false, "module": "/LunaScouterassets/scouting-zmu4BDpw.js", "imports": ["/LunaScouterassets/chunk-NL6KNZEE-BxV1z1Wy.js", "/LunaScouterassets/welcome-D_zvdyIk.js"], "css": [], "clientActionModule": void 0, "clientLoaderModule": void 0, "clientMiddlewareModule": void 0, "hydrateFallbackModule": void 0 } }, "url": "/LunaScouterassets/manifest-5cbae75c.js", "version": "5cbae75c", "sri": void 0 };
 const assetsBuildDirectory = "build\\client";
 const basename = "/";
 const future = { "unstable_middleware": false, "unstable_optimizeDeps": false, "unstable_splitRouteModules": false, "unstable_subResourceIntegrity": false, "unstable_viteEnvironmentApi": false };
@@ -1174,7 +1258,7 @@ const ssr = true;
 const isSpaMode = false;
 const prerender = [];
 const routeDiscovery = { "mode": "lazy", "manifestPath": "/__manifest" };
-const publicPath = "/LunaScouter/";
+const publicPath = "/LunaScouter";
 const entry = { module: entryServer };
 const routes = {
   "root": {
@@ -1185,13 +1269,21 @@ const routes = {
     caseSensitive: void 0,
     module: route0
   },
+  "welcome/welcome": {
+    id: "welcome/welcome",
+    parentId: "root",
+    path: "/",
+    index: void 0,
+    caseSensitive: void 0,
+    module: route1
+  },
   "routes/scouting": {
     id: "routes/scouting",
     parentId: "root",
-    path: void 0,
-    index: true,
+    path: "/scouting",
+    index: void 0,
     caseSensitive: void 0,
-    module: route1
+    module: route2
   }
 };
 export {
---
+++ server/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/LunaScouter/sw.js', { scope: '/LunaScouter/' })})}
