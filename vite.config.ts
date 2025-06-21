a811085271d9f39a65ead8dfaeb5255106345716
Michael <theretroanimatedyt@gmail.com>
2025-06-21 14:16:23 -0400

    added an index

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: {
-    //allowedHosts: ["localhost", "2c4b-73-197-229-72.ngrok-free.app"],
-    //allowedHosts: ["localhost", "aba6-73-197-229-72.ngrok-free.app"],
-    allowedHosts: ["localhost", "2414-73-197-229-72.ngrok-free.app"],
-  }
+  base: "/LunaScouter/", // Set base to repo name for GitHub Pages
+  // server config removed for static build
 });
