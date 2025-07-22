f613d5cec8390cc74c5dcbae5a767e6e7eae1341
RetroAnimatedyt <theretroanimatedyt@gmail.com>
2025-07-22 18:11:45 -0400

    Added github action to publish assets as github pages
    Co-Authored-By: Noah Halstead <nhalstead@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   root: ".",
   publicDir: "public",
+  base: "LunaScouter",
   build: {
     outDir: "dist",
     assetsDir: "assets",
