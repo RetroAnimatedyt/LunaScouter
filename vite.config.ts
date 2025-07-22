29cdf8cdd9fb2951fbfe97133b561e98d9a0be08
RetroAnimatedyt <theretroanimatedyt@gmail.com>
2025-07-22 19:27:28 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  publicDir: "public",
-  base: "/LunaScouter/",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
   },
   plugins: [
     react(),
